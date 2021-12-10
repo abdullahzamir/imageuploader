0919de7fce81d9fc07755c27093f91ba6f152f34
Abdullah Zameer <gc@localhost.localdomain>
2021-12-10 23:13:22 +0500

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from flask import Flask
-
-from db import db_init
-
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filenam
+import os
 app=Flask(__name__)
 
+upload_folder = '/home/gc/Desktop/project/imageuploader/'
+allowed_ext = {'png','jpeg','jpg'}
+app.config['upload_folder']
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in allowed_ext
 
 @app.route('/')
-
 def hello_world():
-    return "hello, world"
+    return "hello, world1"
+
+@app.route('/uploads', methods=['POST'])
+def uploads():
+    file = request.files['file']    
+    if not file:
+        return "no file uploaded" , 400
+    if file and allowed_file(file.filename):
+        filename= secure_filenam(file.filename)
+        file.save(os.path.join(app.config['upload_folder'],filename))
+            
+
+
 
